3686986b61076040260daabfecb6d543584c45d0
pesonaprojects <pesonahosting@gmail.com>
2023-03-13 16:41:55 +0700

    13 Maret 2023

--- github.php
+++ github.php
@@ -1,4 +1,9 @@
 <?php
-shell_exec("git pull");
-echo "Selesai";
+
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /www/wwwroot/waapi.es/ && git pull' );
+}
+
 ?>
