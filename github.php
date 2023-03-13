5f10f51fe96a0f29e30230de43e0fb229f56dc3a
pesonaprojects <pesonahosting@gmail.com>
2023-03-13 13:44:07 +0700

    13 Maret 2023

--- github.php
+++ github.php
@@ -1,16 +1,3 @@
 <?php
-$LOCAL_ROOT         = "/path/to/repo/parent/directory";
-$LOCAL_REPO_NAME    = "REPO_NAME";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:username/reponame.git";
-$BRANCH             = "master";
-
-if ( $_POST['payload'] ) {
-  if( file_exists($LOCAL_REPO) ) {
-    shell_exec("cd {$LOCAL_REPO} && git pull");
-    die("done " . mktime());
-  } else {
-    die("done " . mktime());
-  }
-}
+shell_exec("git pull");
 ?>
